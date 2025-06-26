4703aceaac16f31b1a7b6a8d117041eb50c7cede
Niall Moody <n.moody@abertay.ac.uk>
2025-06-26 12:08:11 +0100

    Switched to correct node version, removed testing port comment.

--- server.js
+++ server.js
@@ -631,7 +631,6 @@ fastify.post('/', (request, reply) => {
 
 //------------------------------------------------------------------------------
 // Run the server and report out to the logs
-console.log(`port = ${port}`);
 fastify.listen({ port: port, host: '0.0.0.0' }, (err, address) => {
 
   if(err) {
